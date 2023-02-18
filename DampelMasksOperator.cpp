8147c7f8fd26b6eda36aa4907b9d08c9ecb69dd3
Sladamos <sladamo@wp.pl>
2023-02-18 10:40:13 +0100

    Ready for SOLID changes

--- DampelMasksOperator.cpp
+++ DampelMasksOperator.cpp
@@ -12,19 +12,4 @@ void DampelMasksOperator::initializeMasks()
 		0, -1, 0,
 		-1, 5, -1,
 		0, -1, 0 }));
-
-	//masks.push_back(Mask(maskSize, {
-	//	2, 1, 0,
-	//	1, 0, -1,
-	//	0, -1, -2 }));
-
-	//masks.push_back(Mask(maskSize, {
-	//	1, 2, 1,
-	//	0, 0, 0,
-	//	-1, -2, -1 }));
-
-	//masks.push_back(Mask(maskSize, {
-	//	0, 1, 2,
-	//	-1, 0, 1,
-	//	-2, -1, 0 }));
 }
