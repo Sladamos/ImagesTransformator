1845478265792466fb41b608c5b512e1827a25d5
Sladamos <sladamo@wp.pl>
2023-05-04 22:47:58 +0200

    Safer options

--- Menu.cpp
+++ Menu.cpp
@@ -23,7 +23,10 @@ void Menu::selectAndExecuteOption()
 {
 	string input = communicator->handleInput();
 	shared_ptr<Option> selectedOption = selectMatchingOption(input);
-	selectedOption->execute();
+	if (selectedOption != nullptr)
+	{
+		selectedOption->execute();
+	}
 }
 
 shared_ptr<Option> Menu::selectMatchingOption(const string& handledInput)
@@ -56,5 +59,5 @@ std::shared_ptr<Option> Menu::selectNamedOption(const std::string& handledInput)
 			return option.second;
 	}
 
-	return namedOptions["Exit"];
+	return nullptr;
 }
