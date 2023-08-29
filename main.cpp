500fb9920868b6ddc803e4131087eadfa195b78a
Sladamos <sladamo@wp.pl>
2023-08-29 10:51:42 +0200

    Config reading

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 //Author: Slawomir Adamowicz
 #include "ConsoleMenu.h"
 #include "ProgramEngine.h"
+#include "json.hpp"
+#include <fstream>
+
+using json = nlohmann::json;
+json readProgramConfig();
 
 int main()
 {
+	json config = readProgramConfig();
 	std::shared_ptr<Menu> menu = std::shared_ptr<Menu>(new ConsoleMenu());
 	ProgramEngine engine(menu);
 	engine.startProgram();
 	return 0;
+}
+
+json readProgramConfig()
+{
+	try
+	{
+		std::ifstream f("config.json");
+		json config = json::parse(f);
+		return config;
+	}
+	catch (const std::exception&)
+	{
+		exit(400);
+	}
 }
